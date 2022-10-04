448606067647b70086444b0ffce5f8df770bbe4c
nickwlong <nickwlong@gmail.com>
2022-10-04 11:16:39 +0100

    Updating the gridArray??

--- src/RunGame.js
+++ src/RunGame.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Square } from './Square';
 
 
 export function RunGame() {
-  const gridArray = new Array(16).fill({
+  const gridArraySetup = new Array(16).fill({
     shipStatus: '0',
     hitStatus: '-'
   }) // Making an array to form the grid.
   // shipStatus to store if a boat is saved in that location
   // hitStatus to store if that location has been hit or not yet. 
 
+  var [gridArray, setGridArray] = useState(gridArraySetup)
   const player2GridArray = []
   const player3GridArray = []
 
   return (
     <div className="player board" id="GameContainer">
       {gridArray.map( // maps through the array and makes a square for each of the elements in the array.
-        (square, index) => (<Square square={`${square}`} index={`${index}`}/>) // these 'tags' of square and index pass into the 'props' within the Square class component
+        (square, index) => (<Square square={square} index={index} gridArray={gridArray} setGridArray={setGridArray}/>) // these 'tags' of square and index pass into the 'props' within the Square class component
         )}
     </div>
   );
--- src/Square.js
+++ src/Square.js
@@ -1,3 +1,4 @@
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -5,7 +6,8 @@ export class Square extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: 'empty'
+      hitStatus: this.props.square.hitStatus,
+      index: this.props.index
     }
     this.handleClick = this.handleClick.bind(this)
   }
@@ -17,16 +19,28 @@ export class Square extends React.Component {
   // 
   
 
-  handleClick() {
-    console.log(this.props.square.shipStatus)
-    console.log(this.props.index)
-    this.setState(prevState => ({
-      status: 'ship'
-    }))
+  handleClick () {
+    console.log(this.props.gridArray)
+
+    let newGridArray = this.props.gridArray.map((square, i) => {
+      console.log(i)
+      if(this.props.index === i){
+        const newSquare = square
+        newSquare.shipStatus = 'ship'
+        console.log(newSquare)
+        return newSquare
+      } else {
+        console.log(square)
+        return square
+      }
+    })
+    console.log(newGridArray)
+    this.props.setGridArray(newGridArray)
+  
   }
 
   render(){
     return(
-      <div className={`square ${this.state.status}`} id={`square_${this.props.index}`} key={`square_${this.props.index}`} onClick={() => {this.handleClick()}}> </div>
+      <div className={`square ${this.props.square.shipStatus}`} id={`square_${this.props.index}`} key={`square_${this.props.index}`} onClick={() => {this.handleClick()}}> </div>
   )
 }}
