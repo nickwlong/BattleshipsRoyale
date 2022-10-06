031c3ef562b9bd53f85f74170f18f9c21b488f33
AntVelc <t.velciu@gmail.com>
2022-10-06 12:08:37 +0100

    computers now selects between opponents and takes a shot

--- client/src/SinglePlayer/GameFlow.js
+++ client/src/SinglePlayer/GameFlow.js
@@ -10,7 +10,7 @@ export function GameFlow(props) {
 
   return(
   <div>
-    <Computer turnState={turnState} setTurnState={setTurnState} />
+    <Computer turnState={turnState} setTurnState={setTurnState} grid1Array={props.play1Grid} setGrid1Array={props.setPlay1Grid} grid2Array={props.play2Grid} setGrid2Array={props.setPlay2Grid} grid3Array={props.play3Grid} setGrid3Array={props.setPlay3Grid}/>
     <row>
       <column>
         <h1>Computer 1's Board</h1>
@@ -34,9 +34,9 @@ export function GameFlow(props) {
         <button onClick={() => {setTurnState('Player1')}}>Reset to Player's Turn</button>
 
         <h1>Your Board</h1>
-        <div className="player board" id="GameContainer">
+        <div className="player2 board" id="GameContainer2">
           {props.play1Grid.map( // maps through the array and makes a square for each of the elements in the array.
-            (square, index) => (<Square square={square} key={`player1Board_${index}`} index={index} play1Grid={props.play1Grid} setPlay1Grid={props.setPlay1Grid} setTurnState={setTurnState} turnState={turnState}/>) // these 'tags' of square and index pass into the 'props' within the Square class component
+            (square, index) => (<SquareOpponent square={square} key={`player2Board_${index}`} index={index} gridArray={props.play1Grid} setGridArray={props.setPlay1Grid} setTurnState={setTurnState} turnState={turnState}/>) // these 'tags' of square and index pass into the 'props' within the Square class component
             )}
         </div>
       </column>
