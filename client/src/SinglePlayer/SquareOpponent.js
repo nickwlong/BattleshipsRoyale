e0e1b5c8571f896b77e73fe388d006d7ddcaf0bc
Oswin <oa.pinto@protonmail.com>
2022-10-07 17:32:52 +0100

    added player can see turn

--- client/src/SinglePlayer/SquareOpponent.js
+++ client/src/SinglePlayer/SquareOpponent.js
@@ -1,43 +1,55 @@
-import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Computer} from './Computer';
-import {checkGameWinner} from './GameFlow';
+import { toHaveStyle } from "@testing-library/jest-dom/dist/matchers";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Computer } from "./Computer";
+import { checkGameWinner } from "./GameFlow";
 
-export var {setTurnState} = true;
-export var {turnState} = true;
+export var { setTurnState } = true;
+export var { turnState } = true;
 
-export class SquareOpponent extends React.Component {  
+export class SquareOpponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       hitStatus: this.props.square.hitStatus,
-      index: this.props.index
-
-    }
-    this.handleClick = this.handleClick.bind(this)
+      index: this.props.index,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   // this.props.index => the index within the grid array
 
   // this.state.status => status of the square in the array
 
-  // 
-  
-// this async function is working with the 'let wait' variable to ensure that React doesnt move on to the next bit of code until 'handleClick' is done! -E
-  async handleClick () {
-    if (this.props.turnState === 'Computer1' || this.props.turnState === 'Computer2') {return null}
+  //
+
+  // this async function is working with the 'let wait' variable to ensure that React doesnt move on to the next bit of code until 'handleClick' is done! -E
+  async handleClick() {
+    if (
+      this.props.turnState === "Computer1" ||
+      this.props.turnState === "Computer2"
+    ) {
+      alert("its not your turn yet!");
+      return null;
+    }
 
     let newGridArray = this.props.gridArray.map((square, i) => {
-      if(this.props.index === i && ["ship","ship1","ship2","ship3","ship4"].includes(this.props.square.shipStatus)){
-        return { ...square, hitStatus: 'hit'}
-        
-      } else if (this.props.index === i && this.props.square.shipStatus === '0') {
-        return { ...square, hitStatus: 'miss'}
-      } else { return square }
-
-    })
-
+      if (
+        this.props.index === i &&
+        ["ship", "ship1", "ship2", "ship3", "ship4"].includes(
+          this.props.square.shipStatus
+        )
+      ) {
+        return { ...square, hitStatus: "hit" };
+      } else if (
+        this.props.index === i &&
+        this.props.square.shipStatus === "0"
+      ) {
+        return { ...square, hitStatus: "miss" };
+      } else {
+        return square;
+      }
+    });
 
     // newGridArray.map((square,i) =>{
 
@@ -49,10 +61,10 @@ export class SquareOpponent extends React.Component {
     // } else if(newGridArray != this.props.gridArray){
 
     // the below code is not going to be processed untill handleClick is done -E
-       let wait = await this.props.setGridArray(newGridArray)
-       this.props.checkGameWinner()
-       console.log(this.props.turnState)
-       this.props.setTurnState('Computer1')
+    let wait = await this.props.setGridArray(newGridArray);
+    this.props.checkGameWinner();
+    console.log(this.props.turnState);
+    this.props.setTurnState("Computer1");
     // }
   }
 
@@ -60,9 +72,18 @@ export class SquareOpponent extends React.Component {
   // Check to see if this square in the player's array has a ship, if so, change hitStatus to hit
   // If it does NOT have a ship, change hitStatus to miss
 
-  render(){
-    return(
-      
-      <div className={`opponent square ${this.props.square.hitStatus}`} id={`play2_square_${this.props.index}`} key={`square_${this.props.index}`} onClick={() => {this.handleClick()}}> </div>
-  )
-}}
+  render() {
+    return (
+      <div
+        className={`opponent square ${this.props.square.hitStatus}`}
+        id={`play2_square_${this.props.index}`}
+        key={`square_${this.props.index}`}
+        onClick={() => {
+          this.handleClick();
+        }}
+      >
+        {" "}
+      </div>
+    );
+  }
+}
