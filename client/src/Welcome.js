ffe768ce6e621cf68b8deedbbdb49cc4522cb52e
EsraK1 <esrakaya165@outlook.com>
2022-10-11 17:45:21 +0100

    min save

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -8,8 +8,8 @@ export function Welcome({changePlayState}){
     <h1>Battleships royale</h1>
       <br></br>
       <br></br>
-      <button id='BtnPlayGame' onClick={() => changePlayState('Singleplayer')}>Singleplayer Game</button>
-      <button id='BtnPlayGame' onClick={() => changePlayState('Multiplayer')}>Multiplayer Game</button>
+      <button className = 'SingleplayerButton' id='BtnPlayGame' onClick={() => changePlayState('Singleplayer')}>Singleplayer Game</button>
+      <button className = 'MultiplayerButton' id='BtnPlayGame' onClick={() => changePlayState('Multiplayer')}>Multiplayer Game</button>
       <br></br>
     </div>
   );
