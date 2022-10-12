3b80cae81714a208ed04da2df69c976bec895359
nickwlong <nickwlong@gmail.com>
2022-10-12 17:09:20 +0100

    Confetti and turn status working

--- client/src/SinglePlayer/GameFlow.js
+++ client/src/SinglePlayer/GameFlow.js
@@ -113,8 +113,7 @@ export function GameFlow(props) {
     console.log("Num Comp 1 ships hit:" + player2Hits);
     console.log("Num Comp 2 ships hit:" + player3Hits)
     
-    let turnHeader = document.getElementById('turnHeader')
-    turnHeader.style.display = 'none'
+  
 
 
     if (
@@ -122,6 +121,9 @@ export function GameFlow(props) {
       (player2Hits >= 3 && player3Hits >= 3) ||
       (player3Hits >= 3 && player1Hits >= 3)
     ) {
+
+      let turnHeader = document.getElementById('turnHeader')
+      turnHeader.style.display = 'none'
 
       if (player1Hits < 3) {
         // set the variable 'winnerConfetti' so confetti can go off when Player 1 wins!
@@ -181,17 +183,24 @@ export function GameFlow(props) {
       }
       }>Play Again</button>)
     }
-  
-  
+    
+  function playwin1(){
+      if (props.turnState === props.username && props.turnState !== "game-over"){
+        console.log('Players go!')
+        return(
+        <h1 className="Player1turn" id='turnHeader'>It's your turn</h1>)
+      }
+      else if (props.turnState !== props.username && props.turnState !== "game-over") {
+        console.log('Opponents go!')
+        return (<h1 className="playerTurn" id='turnHeader'>It's {props.turnState}'s turn</h1>)
+      }}
+
+
 
   return(
   <div id='BoardsContainer'>
-    {props.turnState !== "game-over" ? <h1 className="playerTurn" id='turnHeader'>It's {props.turnState}'s turn!</h1> : CallsWinner(winnerConfetti)}
-          {/* if the 'winnerConfetti is equal to 'Player 1' the CallsWinner function is called*/}
-          {/* {winnerConfetti === props.opponentNames[props.playerIndexState] ? CallsWinner(props.opponentNames[props.playerIndexState]) : ""}
-          {winnerConfetti === props.opponentNames[props.opponent1Index] ? CallsWinner(props.opponentNames[props.opponent1Index]) : ""}
-          {winnerConfetti === props.opponentNames[props.opponent2Index] ? CallsWinner(props.opponentNames[props.opponent2Index]) : ""} */}
-
+    {playwin1()}
+    {props.turnState === 'game-over' ? CallsWinner(winnerConfetti) : ''}
     {props.playState === 'Singleplayer' ? <Computer turnState={props.turnState} checkGameWinner={checkGameWinner} setTurnState={props.setTurnState} grid1Array={props.play1Grid} setGrid1Array={props.setPlay1Grid} grid2Array={props.play2Grid} setGrid2Array={props.setPlay2Grid} grid3Array={props.play3Grid} setGrid3Array={props.setPlay3Grid}/> : ""}
     {/* computer function imports grids and turns state, and functions of setting grids and turn state */}
     <container className='responsive-grids'>
