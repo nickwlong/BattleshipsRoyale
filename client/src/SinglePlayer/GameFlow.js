91e7228b258e6964eb140f8d2734f5bb21e6f4a3
EsraK1 <esrakaya165@outlook.com>
2022-10-12 10:24:08 +0100

    responsive grids

--- client/src/SinglePlayer/GameFlow.js
+++ client/src/SinglePlayer/GameFlow.js
@@ -163,12 +163,13 @@ export function GameFlow(props) {
             <div className='content'>
               <h1>  Winner is  {player}  🎉</h1>
               <br></br>
-              <button onClick ={ () => {
+              <button className="play-Again" onClick ={ () => {
               socket.disconnect()
               socket.connect()
               props.setReadyState('pending')
               props.setPlayState('Welcome')
               }}>Play Again</button>
+              <br></br>
               <br></br>
             </div>
           </div>
@@ -186,7 +187,7 @@ export function GameFlow(props) {
 
     {props.playState === 'Singleplayer' ? <Computer turnState={props.turnState} checkGameWinner={checkGameWinner} setTurnState={props.setTurnState} grid1Array={props.play1Grid} setGrid1Array={props.setPlay1Grid} grid2Array={props.play2Grid} setGrid2Array={props.setPlay2Grid} grid3Array={props.play3Grid} setGrid3Array={props.setPlay3Grid}/> : ""}
     {/* computer function imports grids and turns state, and functions of setting grids and turn state */}
-    <row>
+    <container className='responsive-grid'>
       <column>
         {props.playState === 'Singleplayer' ? <div><h1>Computer 1's Board</h1>
         <div className="player2 board" id="GameContainer2">
@@ -251,8 +252,8 @@ export function GameFlow(props) {
           </div> 
           </div>}
       </column>
-    </row>
-    <row>
+    </container>
+    <container className='responsive-grid'>
       <column>
 
         <h1>Your Board</h1>
@@ -264,7 +265,7 @@ export function GameFlow(props) {
             )}
           </div>
         </column>
-      </row>
+      </container>
     </div>
   );
 }
