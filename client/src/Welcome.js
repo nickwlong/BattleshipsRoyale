1d4437b9e5156f29f3a8b2107aba372f0746dc60
oswin <oa.pinto@protonmail.com>
2022-10-12 16:04:55 +0100

    meep

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -5,12 +5,16 @@ import ReactDOM from 'react-dom';
 export function Welcome({changePlayState}){
   return (
     <div className="welcome" >
-      <br></br>
     <h1>Battleships royale</h1>
-      <button className = 'SingleplayerButton' id='BtnPlayGame' onClick={() => changePlayState('Singleplayer')}>Singleplayer Game</button>
-      <button className = 'MultiplayerButton' id='BtnPlayGame' onClick={() => changePlayState('Multiplayer')}>Multiplayer Game</button>
+      <button className ='SingleplayerButton' id='BtnPlayGame' onClick={() => changePlayState('Singleplayer')}>Singleplayer Game</button>
+      <button className ='MultiplayerButton' id='BtnPlayGame' onClick={() => changePlayState('Multiplayer')}>Multiplayer Game</button>
       <br></br>
+      <br></br>
+    <div className = 'MusicButtons'>
+      <button className ="MusicButton" onClick={() => start()}>♫</button>
+      <button className ="MuteButton" onClick={() => stop()}>🔇</button>
     </div>
+  </div>
   );
   
 }
