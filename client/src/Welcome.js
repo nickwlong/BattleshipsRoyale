201443743dbcc428da8e67e7cfaf92480f7c83dc
EsraK1 <esrakaya165@outlook.com>
2022-10-14 00:22:15 +0100

    another bug fixed

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -11,8 +11,8 @@ export function Welcome(props){
     <div className="welcome" >
     <h1>Battleships royale</h1>
         <div className='Player-mode'>
-          <button className ='SingleplayerButton' id='BtnPlayGameSP' onClick={() => props.setPlayState('Single-player')}>Single player Game</button>
-          <button className ='MultiplayerButton' id='BtnPlayGameMP' onClick={() => props.setPlayState('Multiplayer')}>Multi player Game</button>
+          <button className ='SingleplayerButton' id='BtnPlayGameSP' onClick={() => props.setPlayState('Singleplayer')}>Singleplayer Game</button>
+          <button className ='MultiplayerButton' id='BtnPlayGameMP' onClick={() => props.setPlayState('Multiplayer')}>Multiplayer Game</button>
           <button className ='SingeplayerButton Test' id='BtnPlayGameTest' style={{display: 'none'}} onClick={() => {props.setPlayState('Singleplayer'); props.setTestState('test')}}>Test</button>
         </div>
       <br></br>
